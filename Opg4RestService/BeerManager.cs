9a17c999912edc86b0e973b2d2bcaabcf3c4ab69
KristianSpaqaj <kris49j7@edu.easj.dk>
2021-02-23 16:26:47 +0100

    Add project files.

---
+++ Opg4RestService/BeerManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerClassLibrary;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Opg4RestService
+{
+    public class BeerManager
+    {
+        private static int _nextId = 1;
+        private static List<Beer> Data = new List<Beer>()
+        {
+            new Beer(_nextId++,"Carlsberg",12.5,4.5),
+            new Beer(_nextId++,"Tuborg",14,4.5),
+            new Beer(_nextId++,"Corona",11.5, 4.6),
+            new Beer(_nextId++,"Guinness", 16, 4.2)
+        };
+
+        public List<Beer> GetAll()
+        {
+            return Data;
+        }
+
+        public Beer GetId(int id)
+        {
+            return Data.Find(beer => beer.Id == id);
+        }
+
+        public Beer PostBeer(Beer value)
+        {
+            value.Id = _nextId++;
+            Data.Add(value);
+            return value;
+        }
+
+        public Beer Delete(int id)
+        {
+            Beer beer = Data.Find(beer1 => beer1.Id == id);
+            if (beer == null) return null;
+            Data.Remove(beer);
+            return beer;
+        }
+
+        public Beer Update(int id, Beer value)
+        {
+            Beer beer = Data.Find(beer1 => beer1.Id == id);
+            if (beer == null) return null;
+            beer.Navn = value.Navn;
+            beer.Pris = value.Pris;
+            beer.Abv = value.Abv;
+            return beer;
+        }
+    }
+}
---
+++ Opg4RestService/Controllers/BeerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BeerClassLibrary;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Opg4RestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private BeerManager _manager = new BeerManager();
+
+        // GET: api/<BeerController>
+        [HttpGet]
+        public List<Beer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BeerController>/5
+        [HttpGet("{id}")]
+        public Beer Get(int id)
+        {
+            return _manager.GetId(id);
+        }
+
+        // POST api/<BeerController>
+        [HttpPost]
+        public Beer Post([FromBody] Beer value)
+        {
+            return _manager.PostBeer(value);
+        }
+
+        // PUT api/<BeerController>/5
+        [HttpPut("{id}")]
+        public Beer Put(int id, [FromBody] Beer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<BeerController>/5
+        [HttpDelete("{id}")]
+        public Beer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
